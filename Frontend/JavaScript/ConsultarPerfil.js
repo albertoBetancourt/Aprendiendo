baccf1248456825477df3cc0f209e53ae9833369
YISUSPROGRAMER <jesus.cruzr@autonoma.edu.co>
2018-12-04 17:02:50 -0500

    consultar perfffil

--- Frontend/JavaScript/ConsultarPerfil.js
+++ Frontend/JavaScript/ConsultarPerfil.js
@@ -2,15 +2,15 @@ function consultar() {
     reserva = document.getElementById("reservas").value;
     $.post("../../backend/controllers/ConsultaPerfil.php", {}, function (respuesta) {
         var data = eval(respuesta);
-        $("#n").text(datasUsuario[0].username);
-        $("#c").text(datasUsuario[0].nombres);
-        $("#e").text(datasUsuario[0].apellidos);
-        $("#f").text(datasUsuario[0].cedula);
-        $("#f").text(datasUsuario[0].genero);
-        $("#f").text(datasUsuario[0].fechaNacimiento);
-        $("#f").text(datasUsuario[0].email);
-        $("#f").text(datasUsuario[0].ciudad);
-        $("#f").text(datasUsuario[0].pais);
+        $("#n").text(datasUsuario[0]);
+        $("#c").text(datasUsuario[1]);
+        $("#e").text(datasUsuario[2]);
+        $("#f").text(datasUsuario[3]);
+        $("#f").text(datasUsuario[4]);
+        $("#f").text(datasUsuario[5]);
+        $("#f").text(datasUsuario[6]);
+        $("#f").text(datasUsuario[7]);
+        $("#f").text(datasUsuario[8]);
 
     });
 }
