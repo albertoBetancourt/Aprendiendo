3f2517b022191adf68292032b63f2ae5961f1c23
YISUSPROGRAMER <jesus.cruzr@autonoma.edu.co>
2018-12-04 08:07:27 -0500

    Creacion clase hotel

--- Backend/clases/Usuario.php
+++ Backend/clases/Usuario.php
@@ -15,13 +15,7 @@ class Usuario
     private $fotoPerfil;
 
     private $connect; //Para hacer la conexion con la base de datos
-
-    public function __construct()
-    {
-        $this->connect = new Conexion();
-    }
     
-    /** 
     public function __construct($nombresU, $apellidosU, $usernameU, $cedulaU, $emailU, $contraseniaU, $fechaNacimientoU, $generoU, $ciudadU, $paisU)
     {
         $this->nombres = $nombresU;
@@ -36,68 +30,8 @@ class Usuario
         $this->pais = $paisU;
         $this->connect = new Conexion(); 
     }
-    */
-
-    public function registro()
-    {
-        $Sql="insert into usuarios (genero, username, cedula,  nombre, apellido, password, ciudad, pais, fechanacimiento, email) values 
-        ('".$this->genero."', '".$this->username."', '".$this->cedula."', '".$this->nombres."', '".$this->apellidos."', '".$this->contrasenia."', '".$this->ciudad."', '".$this->pais."', '".$this->fechaNacimiento."', '".$this->email."');";
-        if(!$this->verificarLlavePrimaria())
-        {
-            $info=pg_query($this->connect->getRuta(),$Sql);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
     
-    public function verificarLlavePrimaria()
-    {
-        $Sql="select cedula from usuarios where cedula = '".$this->cedula."' ";
-        $info=pg_query($this->connect->getRuta(),$Sql);
-        if($info)
-        {
-            while($row = pg_fetch_array($info))
-            {
-                $id = $row['cedula'];
-                if($id==$this->cedula)
-                {
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-        }
-    }
 
-    public function verificarContrasena($nombreUsuario, $contrasenaRecibida)
-    {
-        $Sql="select * from usuarios where username = '".$nombreUsuario."' ";
-        $info=pg_query($this->connect->getRuta(),$Sql);
-        if(!$info)
-            return json_encode("Error");
-        else
-        {
-            while ($row = pg_fetch_array($info))
-            {
-                $contrasenaReal = $row['password'];
-                if($contrasenaReal == $contrasenaRecibida)
-                {
-                    session_start();
-                    $_SESSION["usuarios"]["nombre"] = $row["nombre"];
-                    $_SESSION["usuarios"]["cedula"]= $row["cedula"];
-                    $_SESSION["usuarios"]["email"] = $row["email"];
-                    $_SESSION["usuarios"]["creado"] = true;
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-    }
+    
 }
 ?>
